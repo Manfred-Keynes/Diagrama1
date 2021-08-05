5f9db217c35fd70192c8a5a6a2f87eea7230d7dc
Manfred-Keynes <mfer14211@gmail.com>
2021-08-04 23:31:13 -0600

    Diagrama1

---
+++ Diagrama1/src/modelo/Avion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author gerso
+ */
+public class Avion {
+     
+    private String Categoria;
+    private int numeronumeroTurbinas;
+    
+    
+    public void agregar(){}
+    public void modificar(){}
+    public void elimnar(){}
+    public void listar (){}
+    
+}
